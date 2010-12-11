19942ba50ccaad72ccd4c1f6a71dc958781a5599
wucan <can_wu@cnbvcom.com>
2010-12-11 15:51:26 +0800

    pid_map_rule: fix pid map range according 2010-12-5 doc

--- src/pid_map_rule.c
+++ src/pid_map_rule.c
@@ -6,10 +6,12 @@
 
 #define defProgPidBgn		0x100
 
+#define PID_MAP_MAX_PID_NUM_PER_CHAN		0x200
+#define PID_MAP_MAX_PID_NUM_PER_PROG		0x10
 
 uint16_t pid_map_rule_map_psi_pid(uint8_t chan_idx, uint8_t prog_idx, enmDswPsiPid psi_pid)
 {
-	return defProgPidBgn + chan_idx * PROGRAM_PID_MAX_NUM +
-		PROGRAM_MAX_NUM * prog_idx + psi_pid;
+	return defProgPidBgn + chan_idx * PID_MAP_MAX_PID_NUM_PER_CHAN +
+		PID_MAP_MAX_PID_NUM_PER_PROG * prog_idx + psi_pid;
 }
 
