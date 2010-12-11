659123c95621390d4ce719a852165d59c3f5e756
wucan <can_wu@cnbvcom.com>
2010-12-11 20:38:12 +0800

    move gen_fpga_pid_map_fr_mcu() to pid_map_table.c

--- src/pid_map_table.c
+++ src/pid_map_table.c
@@ -4,6 +4,7 @@
 
 #include "hfpga.h"
 #include "pid_map_table.h"
+#include "front_panel_intstr.h"
 
 
 static msgobj mo = {MSG_INFO, ENCOLOR, "pid_map_table"};
@@ -36,5 +37,50 @@ int pid_map_table_apply(void *pid_map_data, int size)
 	memcpy(pid_map.pid_map, pid_map_data, sizeof(pid_map.pid_map));
 
 	hfpga_write_pid_map(pid_map);
+}
+
+int gen_fpga_pid_map_fr_mcu(uint8_t * pbuf, PROG_INFO_T * pProgpara)
+{
+	uint8_t buf[PROGRAM_MAX_NUM * (4 + PROGRAM_DATA_PID_MAX_NUM * 2) * sizeof(uint16_t)];
+	int i;
+	int nProgSel = 0;
+	PROG_INFO_T *pProg = pProgpara;
+	memset(buf, 0x0F, sizeof(buf));
+
+	for (i = 0; i < CHANNEL_MAX_NUM * PROGRAM_MAX_NUM; i++) {
+		int j;
+		int noff = nProgSel * (4 + PROGRAM_DATA_PID_MAX_NUM * 2) * sizeof(uint16_t);
+		pProg = pProgpara + i;
+		if (pProg->status == 1) {
+			conv_progpid_to_buf(&buf[noff + 0], pProg->PMT_PID_IN);
+			conv_progpid_to_buf(&buf[noff + 2], pProg->PMT_PID_OUT);
+			conv_progpid_to_buf(&buf[noff + 4], pProg->PCR_PID_IN);
+			conv_progpid_to_buf(&buf[noff + 6], pProg->PCR_PID_OUT);
+
+			for (j = 0; j < PROGRAM_DATA_PID_MAX_NUM; j++) {
+				conv_progpid_to_buf(&buf[noff + 8 + j * 4],
+									pProg->pids[j].in);
+				conv_progpid_to_buf(&buf[noff + 10 + j * 4],
+									pProg->pids[j].out);
+			}
+			nProgSel++;
+			if (nProgSel >= PROGRAM_MAX_NUM)
+				break;
+		}
+	}
+
+	for (i = 0;
+		 i < PROGRAM_MAX_NUM * (4 + PROGRAM_DATA_PID_MAX_NUM * 2) * sizeof(uint16_t);
+		 i++) {
+		if (i % 0x10 == 0)
+			printf("\n");
+		printf("%02X ", buf[i]);
+	}
+	printf("\ngen_fpga_pid_map_fr_mcu_buf_len=%d\n", sizeof(buf));
+
+	memcpy(pbuf, buf, sizeof(buf));
+	printf("gen_fpga_pid_map_fr_mcu_buf_len=%d\n", sizeof(buf));
+
+	return 0;
 }
 
