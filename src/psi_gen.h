dabafb8b30c55ac39ec61e7adda617681bf5999d
wucan <can_wu@cnbvcom.com>
2010-12-12 11:13:47 +0800

    psi_gen: make sdt_gen_context api more wise

--- src/psi_gen.h
+++ src/psi_gen.h
@@ -11,26 +11,35 @@
 /*
  * SDT generate stuff
  */
+struct sdt_service_info {
+	char name[PROGRAM_NAME_SIZE];
+	uint8_t name_len;
+};
 struct sdt_gen_context {
 	uint8_t serv_num;
 	uint8_t desc_num;
+
+	struct sdt_service_info serv_info[PROGRAM_MAX_NUM];
 
 	uv_sdt_data sdt_data;
 	uv_sdt_serv_data sdt_serv_data[PROGRAM_MAX_NUM];
 };
 static inline void sdt_gen_context_init(struct sdt_gen_context *ctx)
 {
-	int i, j;
-	uv_descriptor *p_descr;
-	uv_sdt_serv_data *sdt_serv_data = ctx->sdt_serv_data;
-	static uint8_t sdbuf[PROGRAM_MAX_NUM][5][UV_DESCR_LEN];
-
-	ctx->serv_num = PROGRAM_MAX_NUM;
-	ctx->desc_num = 2;
+	ctx->serv_num = 0;
+	ctx->desc_num = 1;
 
 	ctx->sdt_data.i_table_id = 0x42;
 	ctx->sdt_data.i_tran_stream_id = 0x1234;
 	ctx->sdt_data.i_orig_net_id = 0xEAEA;
+
+}
+static inline void sdt_gen_context_pack(struct sdt_gen_context *ctx)
+{
+	int i, j;
+	uv_descriptor *p_descr;
+	uv_sdt_serv_data *sdt_serv_data = ctx->sdt_serv_data;
+	static uint8_t sdbuf[PROGRAM_MAX_NUM][5][UV_DESCR_LEN];
 
 	for (i = 0; i < ctx->serv_num; i++) {
 		sdt_serv_data[i].i_serv_id = i + 1;
@@ -49,19 +58,20 @@ static inline void sdt_gen_context_init(struct sdt_gen_context *ctx)
 			p_descr[j].p_data = sdbuf[i][j];
 			p_descr[j].p_data[0] = 0x01; // serivece type: DTV
 			p_descr[j].p_data[1] = 4;
-			p_descr[j].p_data[6] = 7;
-			if (j == 0) {
-				sprintf(name, "CCTV-%02d", i);
-				memcpy(p_descr[j].p_data + 2, "CCTV", 4);
-				memcpy(p_descr[j].p_data + 7, name, 7);
-			} else {
-				sprintf(name, "SZTV-%02d", i);
-				memcpy(p_descr[j].p_data + 2, "SZTV", 4);
-				memcpy(p_descr[j].p_data + 7, name, 7);
-			}
+			p_descr[j].p_data[6] = ctx->serv_info[i].name_len;
+			memcpy(p_descr[j].p_data + 2, "CCTV", 4);
+			memcpy(p_descr[j].p_data + 7, ctx->serv_info[i].name, ctx->serv_info[i].name_len);
 		}
 	}
+}
+static inline void sdt_gen_context_add_service(struct sdt_gen_context *ctx,
+		const char *serv_name)
+{
+	struct sdt_service_info *info = &ctx->serv_info[ctx->serv_num];
 
+	info->name_len = strlen(serv_name);
+	memcpy(info->name, serv_name, info->name_len);
+	ctx->serv_num++;
 }
 static inline void sdt_gen_context_free(struct sdt_gen_context *ctx)
 {
