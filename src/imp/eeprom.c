093e3a4b5138f8d0d716b3559327385d76bff387
wucan <can_wu@cnbvcom.com>
2010-12-12 22:05:31 +0800

    eeprom: implement file backed EEPROM in !_UCLINUX_

--- src/imp/eeprom.c
+++ src/imp/eeprom.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 #include "eeprom.h"
 
 
+#define EEPROM_SIZE					(1024 * 128)
+
+#ifdef _UCLINUX_
 #define UV_HAT24C64_DEV_PATH		"/dev/AT24C64"
+#else
+#define UV_HAT24C64_DEV_PATH		"./EEPROM"
+#endif
 
 static int eep_fd;
 int eeprom_open()
 {
 	eep_fd = open(UV_HAT24C64_DEV_PATH, O_RDWR);
 	if (eep_fd < 0) {
+#ifdef _UCLINUX_
 		return -1;
+#else
+		/*
+		 * create a new file backed eeprom if not exist
+		 */
+		eep_fd = open(UV_HAT24C64_DEV_PATH, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
+		if (eep_fd < 0) {
+			perror("open");
+			return -1;
+		}
+		/* init to zero of all size */
+		posix_fallocate(eep_fd, 0, EEPROM_SIZE);
+#endif
 	}
 
 	return 0;
