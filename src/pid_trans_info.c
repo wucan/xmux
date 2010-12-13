5706df146acafec7a01dd456c1a0b8275c167453
wucan <can_wu@cnbvcom.com>
2010-12-13 22:58:51 +0800

    pid_trans_info: fix snmp read/write codes

--- src/pid_trans_info.c
+++ src/pid_trans_info.c
@@ -9,37 +9,38 @@
 
 struct pid_trans_info_snmp_data sg_mib_pid_trans_info[CHANNEL_MAX_NUM];
 
+#define NODE_MAX_SIZE			1422
+struct access_data {
+	uint8_t node[3][NODE_MAX_SIZE];
+};
+
 void pid_trans_info_read_data_snmp(uint8_t trans_idx, struct wu_snmp_value *v)
 {
-	uint8_t chan_idx = (trans_idx - 1) / 3;
-	int idx = (trans_idx - 1) % 3;
-	uint8_t *mib_data = (uint8_t *)&sg_mib_pid_trans_info[chan_idx];
-	uint16_t mib_data_len = sg_mib_xxx_len(mib_data);
-	int cnt = mib_data_len / 1422;
+	uint8_t chan_idx = trans_idx / 3;
+	int idx = trans_idx % 3;
+	uint16_t mib_data_len = sg_mib_pid_trans_info[chan_idx].data_len + 2;
+	struct access_data *d = (struct access_data *)&sg_mib_pid_trans_info[chan_idx];
 
-	v->data = mib_data + idx * 1422;
-	v->size = 1422;
+	v->data = d->node[idx];
+	if (idx < mib_data_len / NODE_MAX_SIZE) {
+		v->size = NODE_MAX_SIZE;
+	} else {
+		v->size = mib_data_len % NODE_MAX_SIZE;
+	}
 }
 
 void pid_trans_info_write_data_snmp(uint8_t trans_idx, struct wu_snmp_value *v)
 {
-	uint8_t chan_idx = (trans_idx - 1) / 3;
-	bool chan_start = (trans_idx % 3) == 1;
-	uint8_t *mib_data = (uint8_t *)&sg_mib_pid_trans_info[chan_idx];
-	uint16_t write_off = 0;
+	uint8_t chan_idx = trans_idx / 3;
+	int idx = trans_idx % 3;
+	struct access_data *d = (struct access_data *)&sg_mib_pid_trans_info[chan_idx];
 
-	if (chan_start) {
-		write_off = 0;
-	} else {
-		write_off = sg_mib_xxx_len(mib_data) + 2;
-	}
-	memcpy(mib_data + write_off, v->data, v->size);
-	sg_mib_xxx_update_len(mib_data, write_off + v->size - 2);
+	memcpy(d->node[idx], v->data, v->size);
 
 	/*
 	 * save to eeprom if channel's pid trans info all got
 	 */
-	if (v->size < 1422 || chan_idx == 2) {
+	if (v->size < NODE_MAX_SIZE || chan_idx == 2) {
 		eeprom_write(EEPROM_OFF_PID_TRANS_INFO + PID_TRANS_INFO_SIZE * chan_idx,
 			&sg_mib_pid_trans_info[chan_idx], PID_TRANS_INFO_SIZE);
 	}
