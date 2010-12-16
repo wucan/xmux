5a46e4b11e538b47a8277623ed45e268d83d2fc6
wucan <can_wu@cnbvcom.com>
2010-12-16 17:48:02 +0800

    add xmux_misc module
    At first define the function of set output_bitrate

---
+++ src/xmux_misc.c
@@ -0,0 +1,12 @@
+#include "xmux.h"
+#include "xmux_config.h"
+#include "xmux_misc.h"
+
+
+void set_output_bitrate(uint32_t new_bitrate)
+{
+	/* TODO: set */
+
+	xmux_config_save_output_bitrate(new_bitrate);
+}
+
---
+++ src/xmux_misc.h
@@ -0,0 +1,11 @@
+#ifndef _XMUX_MISC_H_
+#define _XMUX_MISC_H_
+
+#include "wu/wu_base_type.h"
+
+
+void set_output_bitrate(uint32_t new_bitrate);
+
+
+#endif /* _XMUX_MISC_H_ */
+
