ae8bfcf125aabc8bd0ef80f0ed26b6e19749e770
wucan <can_wu@cnbvcom.com>
2010-12-25 14:40:44 +0800

    wu_csc: silence wu_csc() caller warning
    make wu_csc() take void * as buf type

--- include/wu/wu_csc.h
+++ include/wu/wu_csc.h
@@ -4,13 +4,14 @@
 /*
  * calculate check sum
  */
-static inline uint8_t wu_csc(uint8_t *buf, int len)
+static inline uint8_t wu_csc(void *buf, int len)
 {
 	int i;
 	uint8_t csc = 0;
+	uint8_t *p = (uint8_t *)buf;
 
 	for (i = 0; i < len; i++)
-		csc += buf[i];
+		csc += p[i];
 
 	return csc;
 }
