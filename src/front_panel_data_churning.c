d8d374b6c6f9478864eb158e3c65b799a5eecffa
wucan <can_wu@cnbvcom.com>
2010-12-25 15:56:41 +0800

    fp_data_churning: check pid validate in pid_trans_info conversion

--- src/front_panel_data_churning.c
+++ src/front_panel_data_churning.c
@@ -114,6 +114,12 @@ void pid_trans_info_2_prog_info_of_channel(uint8_t chan_idx)
 		for (pid_idx = 0; pid_idx < PROGRAM_DATA_PID_MAX_NUM; pid_idx++) {
 			in_pid = xmux_prog->data[pid_idx].in;
 			out_pid = xmux_prog->data[pid_idx].out;
+			if (!data_pid_validate(in_pid)) {
+				fp_prog->data[pid_idx].in = DATA_PID_PAD_VALUE;
+				fp_prog->data[pid_idx].out = DATA_PID_PAD_VALUE;
+				fp_prog->data[pid_idx].type = 0;
+				continue;
+			}
 			fp_prog->data[pid_idx].in = DATA_PID_VALUE(in_pid);
 			fp_prog->data[pid_idx].out = DATA_PID_VALUE(out_pid);
 			fp_prog->data[pid_idx].type = pid_type_xmux_2_es(DATA_PID_TYPE(in_pid));
@@ -171,6 +177,11 @@ void prog_info_2_pid_trans_info_of_channel(uint8_t chan_idx)
 		for (pid_idx = 0; pid_idx < PROGRAM_DATA_PID_MAX_NUM; pid_idx++) {
 			in_pid = fp_prog->data[pid_idx].in;
 			out_pid = fp_prog->data[pid_idx].out;
+			if (in_pid == DATA_PID_PAD_VALUE && out_pid == DATA_PID_PAD_VALUE) {
+				xmux_prog->data[pid_idx].in = DATA_PID_PAD_VALUE;
+				xmux_prog->data[pid_idx].out = DATA_PID_PAD_VALUE;
+				continue;
+			}
 			pid_type = pid_type_es_2_xmux(fp_prog->data[pid_idx].type);
 			xmux_prog->data[pid_idx].in = PACK_DATA_PID(pid_type, in_pid);
 			xmux_prog->data[pid_idx].out = PACK_DATA_PID(pid_type, out_pid);
