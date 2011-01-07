998b2366b12d3f25a6ca85ede07b9e9146f5afff
wucan <can_wu@cnbvcom.com>
2011-01-07 09:06:58 +0800

    bump version 0.02

--- include/xmux.h
+++ include/xmux.h
@@ -183,11 +183,11 @@ struct xmux_system_param {
 #define XMUX_VENDOR			"SOUKA"
 
 #define XMUX_VERSION_MAJOR	0
-#define XMUX_VERSION_MINOR	1
+#define XMUX_VERSION_MINOR	2
 #define XMUX_VERSION_NUM	((XMUX_VERSION_MAJOR << 16) | XMUX_VERSION_MINOR)
-#define XMUX_VERSION_STR	"0.01"
+#define XMUX_VERSION_STR	"0.02"
 
-#define XMUX_UPDATE_TIME	((2010 << 16) | (12 << 8) | 5)
+#define XMUX_UPDATE_TIME	((2011 << 16) | (1 << 8) | 3)
 
 /*
  * net parameter
