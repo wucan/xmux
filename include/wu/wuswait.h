e3b8a30a7657f29e37a24240e314e787c74a2fbe
wucan <can_wu@cnbvcom.com>
2011-03-04 14:26:51 +0800

    wu: add wuswait small/sleep wait

---
+++ include/wu/wuswait.h
@@ -0,0 +1,21 @@
+#ifndef _WU_SWAIT_H_
+#define _WU_SWAIT_H_
+
+#include "lock.h"
+
+
+typedef struct wu_swait {
+	Lock lock;
+	int alive;
+	int setted;
+	void *data;
+} WuSWait;
+
+void wu_swait_init(WuSWait *swait);
+void wu_swait_destroy(WuSWait *swait);
+void * wu_swait_timedwait(WuSWait *swait, int usec);
+int wu_swait_wakeup(WuSWait *swait, void *data);
+
+
+#endif /* _WU_SWAIT_H_ */
+
---
+++ src/libwu/wuswait.c
@@ -0,0 +1,57 @@
+#include "wuswait.h"
+#include "lock.h"
+
+
+void wu_swait_init(WuSWait *swait)
+{
+	lock_init(&swait->lock);
+	swait->alive = 0;
+	swait->setted = 0;
+}
+
+void wu_swait_destroy(WuSWait *swait)
+{
+	lock_destroy(&swait->lock);
+}
+
+void * wu_swait_timedwait(WuSWait *swait, int usec)
+{
+	void *data = NULL;
+	int cnt = usec / 10000;
+
+	if (cnt <= 0)
+		cnt = 1;
+
+	lock_lock(&swait->lock);
+	swait->alive = 1;
+	swait->setted = 0;
+	lock_unlock(&swait->lock);
+
+	while (cnt > 0) {
+		if (swait->setted) {
+			break;
+		}
+		usleep(10000);
+	}
+
+	lock_lock(&swait->lock);
+	swait->alive = 0;
+	swait->setted = 0;
+	lock_unlock(&swait->lock);
+
+	return swait->data;
+}
+
+int wu_swait_wakeup(WuSWait *swait, void *data)
+{
+	if (!swait->alive || swait->setted)
+		return -1;
+
+	lock_lock(&swait->lock);
+	swait->data = data;
+	swait->setted = 1;
+	lock_unlock(&swait->lock);
+
+	return 0;
+}
+
