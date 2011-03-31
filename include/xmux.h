915c7a1e9a07d73a7b6ba56320c6a576403ddf25
wucan <wu.canus@gmail.com>
2011-03-31 21:24:26 +0800

    0.09

--- include/xmux.h
+++ include/xmux.h
@@ -198,11 +198,11 @@ struct xmux_system_param {
 #define XMUX_VENDOR			"SOUKA"
 
 #define XMUX_VERSION_MAJOR	0
-#define XMUX_VERSION_MINOR	8
+#define XMUX_VERSION_MINOR	9
 #define XMUX_VERSION_NUM	((XMUX_VERSION_MAJOR << 16) | XMUX_VERSION_MINOR)
-#define XMUX_VERSION_STR	"0.08"
+#define XMUX_VERSION_STR	"0.09"
 
-#define XMUX_UPDATE_TIME	((2011 << 16) | (3 << 8) | 27)
+#define XMUX_UPDATE_TIME	((2011 << 16) | (3 << 8) | 31)
 
 /*
  * net parameter
