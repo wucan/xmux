ea947e3ee2fae1abb03673ed8ec15cd2a8992e79
Can Wu <wu.canus@gmail.com>
2011-06-04 13:44:26 +0800

    xmux_tuner: #if 0 of test code for set tuner param

--- src/xmux_tunner.c
+++ src/xmux_tunner.c
@@ -23,6 +23,14 @@ int tunner_set_param(int id, struct tunner_param *param)
 {
 	int len, rc;
 
+#if 0
+	uint8_t work_param[] = {0x74, 01, 01, 00, 0x0f, 0x60, 0xe7, 0xbc, 0x00,
+		0xe0, 0x9d, 0xa3, 0x01, 0xa0, 0x6c, 0xac, 0x00, 0x01, 0x1, 01, 0x00};
+
+	ctrl_mcu_send_cmd(work_param, sizeof(work_param));
+	return 0;
+#endif
+
 	len = fp_build_cmd(send_buf, false, CTRL_MCU_CMD_TUNNER1_PARAM + id,
 			param, sizeof(*param));
 	ctrl_mcu_send_cmd(send_buf, len);
