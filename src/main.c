876ee4efd816e12eed36ff01dff655e5a429a2c5
Can Wu <wu.canus@gmail.com>
2011-06-05 11:11:42 +0800

    dump out how many program per channel we supported

--- src/main.c
+++ src/main.c
@@ -24,7 +24,8 @@ static void restore_work_field();
 int main(int argc, char **argv)
 {
 	trace_info("xmux %s", XMUX_VERSION_STR);
-	trace_info("support %d channels", CHANNEL_MAX_NUM);
+	trace_info("support %d channels, %d programs",
+		CHANNEL_MAX_NUM, PROGRAM_MAX_NUM);
 
 	mem_init_size(1024 * 1024 * 3);
 
