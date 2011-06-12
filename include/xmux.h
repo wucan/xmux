77263fe5252f181f0e1ef025fd93b70eac22da17
Can Wu <wu.canus@gmail.com>
2011-06-12 13:39:46 +0800

    0.21

--- include/xmux.h
+++ include/xmux.h
@@ -203,11 +203,11 @@ struct xmux_system_param {
 #define XMUX_VENDOR			"SOUKA"
 
 #define XMUX_VERSION_MAJOR	0
-#define XMUX_VERSION_MINOR	20
+#define XMUX_VERSION_MINOR	21
 #define XMUX_VERSION_NUM	((XMUX_VERSION_MAJOR << 16) | XMUX_VERSION_MINOR)
-#define XMUX_VERSION_STR	"0.20"
+#define XMUX_VERSION_STR	"0.21"
 
-#define XMUX_UPDATE_TIME	((2011 << 16) | (6 << 8) | 5)
+#define XMUX_UPDATE_TIME	((2011 << 16) | (6 << 8) | 11)
 
 /*
  * net parameter
