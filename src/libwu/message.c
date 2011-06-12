54db22224a3bbcb36c96d4b53e99527f832b1e8e
Can Wu <wu.canus@gmail.com>
2011-06-12 17:50:44 +0800

    libwu: hex_dump() don't print "0x" preffix

--- src/libwu/message.c
+++ src/libwu/message.c
@@ -148,7 +148,7 @@ void hex_dump(const char *name, unsigned char *buf, int size)
 	if (size <= 0)
 		return;
 
-	printf("%s(buf %p, size %d): 0x\n", name, buf, size);
+	printf("%s(buf %p, size %d):\n", name, buf, size);
 	for (i = 0; i < size; i++) {
 		printf("%02x ", *buf);
 		if ((i + 1) % 16 == 0)
