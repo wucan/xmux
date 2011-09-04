77c306473225d1ea221b881c4ece0f5b6a9c65c5
Can Wu <wu.canus@gmail.com>
2011-09-04 13:12:18 +0800

    build-arm.sh: clean CFLAGS & LDFLAGS

--- build-arm.sh
+++ build-arm.sh
@@ -29,8 +29,16 @@ echo "configure for mux with ${CHANNEL} channels ..."
 
 
 CC=arm-elf-gcc \
-CFLAGS="-I`pwd`/extern-arm-libs/net-snmp/include -D_UCLINUX_ -O2 -g -fomit-frame-pointer   -Dlinux -D__linux__ -Dunix -D__uClinux__ -DEMBED -I${SNAPGEAR}/lib/uClibc/include -I${SNAPGEAR}/lib/libm -I${SNAPGEAR}/lib/libcrypt_old -I${SNAPGEAR} -fno-builtin -nostartfiles -I${SNAPGEAR}/linux-2.4.x/include" \
-LDFLAGS="-L`pwd`/net-snmp-lib/lib/ -O2 -g -fomit-frame-pointer   -Dlinux -D__linux__ -Dunix -D__uClinux__ -DEMBED -I${SNAPGEAR}/lib/uClibc/include -I${SNAPGEAR}/lib/libm -I${SNAPGEAR}/libcrypt_old -I${SNAPGEAR} -fno-builtin -nostartfiles -I${SNAPGEAR}/linux-2.4.x/include -Wl,-elf2flt  -L${SNAPGEAR}/lib/uClibc/lib -L${SNAPGEAR}/lib/libm -L${SNAPGEAR}/lib/libnet -L${SNAPGEAR}/lib/libdes -L${SNAPGEAR}/lib/libaes -L${SNAPGEAR}/lib/libpcap -L${SNAPGEAR}/lib/libcrypt_old -L${SNAPGEAR}/lib/libssl -L${SNAPGEAR}/lib/zlib -lpthread" \
+CFLAGS="-D_UCLINUX_ -g -Dlinux -D__linux__ -Dunix -D__uClinux__ -DEMBED \
+	-I`pwd`/extern-arm-libs/net-snmp/include \
+	-I${SNAPGEAR}/lib/uClibc/include -I${SNAPGEAR}/lib/libm \
+	-I${SNAPGEAR}/lib/libcrypt_old -I${SNAPGEAR} -I${SNAPGEAR}/linux-2.4.x/include" \
+LDFLAGS="-O2 -fomit-frame-pointer -fno-builtin -nostartfiles -Wl,-elf2flt \
+	-L`pwd`/net-snmp-lib/lib/ \
+	-L${SNAPGEAR}/lib/uClibc/lib -L${SNAPGEAR}/lib/libm \
+	-L${SNAPGEAR}/lib/libnet -L${SNAPGEAR}/lib/libdes -L${SNAPGEAR}/lib/libaes \
+	-L${SNAPGEAR}/lib/libpcap -L${SNAPGEAR}/lib/libcrypt_old \
+	-L${SNAPGEAR}/lib/libssl -L${SNAPGEAR}/lib/zlib -lpthread" \
 ./configure --host=i686 --with-build-target=arm --with-channel=${CHANNEL} \
 	--with-snapgear-dir=${SNAPGEAR}
 
