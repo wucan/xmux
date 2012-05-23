5a1362e2fd113cacae68b28826b2e6bda8a47402
Can Wu <wu.canus@gmail.com>
2012-05-23 21:45:35 +0800

    add libci simulation codes
    make the sim program compiling and working.

---
+++ src/sim_libci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+unsigned int ci_max_cnt[2];
+
+void get_ci_name(char *buf)
+{
+	snprintf(buf, 20, "%s", "CI Name");
+}
+
+void loadPMT(uint8_t *sec[], int sec_len[], int num)
+{
+}
+
+void libci_test()
+{
+}
+
