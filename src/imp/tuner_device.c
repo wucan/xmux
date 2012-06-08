e537d4c01e30da98c406ba6f763ac64902afc3de
Can Wu <wu.canus@gmail.com>
2012-06-07 18:20:45 -0700

    tunner_device: remove dead codes of i2c access interface
    dlh had use the gpio interface.

--- src/imp/tuner_device.c
+++ src/imp/tuner_device.c
@@ -20,7 +20,6 @@
 #define F22K_GPIO			53
 #define TUNER_SDA 49
 #define TUNER_SCL 44
-static int fd;
 typedef enum
 	{
 		I2C_OK = 0,		/* I2C transmission OK */
@@ -254,128 +253,12 @@ void tuner_device_f22k(unsigned char mode)
 
 int tuner_device_open()
 {
-	if (fd > 0)
-		return 0;
-
 	tuner_device_reset();
-
-	fd = open(TUNER_DEV, O_RDWR);
-	if (fd < 0) {
-		printf("can't open device %s! try %s\n", TUNER_DEV, TUNER_DEV1);
-		fd = open(TUNER_DEV1, O_RDWR);
-		if (fd < 0) {
-			printf("can't open device %s!\n", TUNER_DEV1);
-			return -1;
-		}
-	}
 
 	return 0;
 }
 
 void tuner_device_close()
 {
-	if (fd > 0) {
-		close(fd);
-		fd = 0;
-	}
-}
-
-int tuner_device_write_register(uint16_t reg, uint8_t *buf, size_t size)
-{
-	struct i2cdev_ioc_data_param param;
-	int rc;
-
-	if (fd <= 0)
-		return -1;
-
-	if (size > (TUNER_MAX_RW_BYTES - 3)) {
-		fprintf(stderr, "tuner: write exceed! %d > %d\n",
-			size, TUNER_MAX_RW_BYTES - 3);
-		return -1;
-	}
-
-	param.nbytes_write = 3 + size;
-	param.bytes[0] = TUNER_W_ADDR;
-	param.bytes[1] = reg >> 8;
-	param.bytes[2] = reg & 0xFF;
-	memcpy(&param.bytes[3], buf, size);
-	rc = ioctl(fd, I2CDEV_IOC_WRITE_DATA, &param);
-
-	return 0;
-}
-
-int tuner_device_read_register(uint16_t reg, uint8_t *buf, size_t size)
-{
-	struct i2cdev_ioc_data_param param;
- 	int rc;
-
-	if (fd <= 0)
-		return -1;
-
-	if (size > (TUNER_MAX_RW_BYTES - 3)) {
-		fprintf(stderr, "tuner: read exceed! %d > %d\n",
-			size, TUNER_MAX_RW_BYTES - 3);
-		return -1;
-	}
-	param.nbytes_write = 3;
-	param.nbytes_expect_read = size;
-	param.bytes[0] = TUNER_W_ADDR;
-	param.bytes[1] = reg >> 8;
-	param.bytes[2] = reg & 0xFF;
-	param.read_head_nbytes = 1;
-	param.read_head_bytes[0] = TUNER_R_ADDR;
-	rc = ioctl(fd, I2CDEV_IOC_WREAD_DATA, &param);
-	printf("reg %#x data: 0x%x 0x%x 0x%x\n", reg,
-		param.bytes[0], param.bytes[1], param.bytes[2]);
-	memcpy(buf, param.bytes, size);
-
-	return 0;
-}
-
-int tuner_device_6110_write_register(uint16_t reg, uint8_t *buf, size_t size)
-{
-        struct i2cdev_ioc_data_param param;
-        int rc;
-
-        if (fd <= 0)
-                return -1;
-
-        if (size > (TUNER_MAX_RW_BYTES - 2)) {
-                fprintf(stderr, "tuner: write exceed! %d > %d\n",
-                        size, TUNER_MAX_RW_BYTES - 2);
-                return -1;
-        }
-
-        param.nbytes_write = 2 + size;
-        param.bytes[0] = TUNER_6110_W_ADDR;
-        param.bytes[1] = reg &0x00ff;
-        memcpy(&param.bytes[2], buf, size);
-        rc = ioctl(fd, I2CDEV_IOC_WRITE_DATA, &param);
-
-        return 0;
-}
-int tuner_device_6110_read_register(uint16_t reg, uint8_t *buf, size_t size)
-{
-        struct i2cdev_ioc_data_param param;
-        int rc;
-
-        if (fd <= 0)
-            return -1;
-
-        if (size > (TUNER_MAX_RW_BYTES - 2)) {
-                fprintf(stderr, "tuner: read exceed! %d > %d\n",
-                        size, TUNER_MAX_RW_BYTES - 2);
-                return -1;
-        }
-
-        param.nbytes_write = 2;
-        param.nbytes_expect_read = size;
-        param.bytes[0] = TUNER_6110_R_ADDR;
-        param.bytes[1] = reg&0x00FF;
-        param.read_head_nbytes = 0;
-        rc = ioctl(fd, I2CDEV_IOC_WREAD_DATA, &param);
-        memcpy(buf, param.bytes, size);
-
-        return 0;
 }
 
--- src/imp/tuner_device.h
+++ src/imp/tuner_device.h
@@ -7,11 +7,6 @@
 void tuner_device_reset();
 int tuner_device_open();
 void tuner_device_close();
-int tuner_device_write_register(uint16_t reg, uint8_t *buf, size_t size);
-int tuner_device_read_register(uint16_t reg, uint8_t *buf, size_t size);
-int tuner_device_6110_write_register(uint16_t reg, uint8_t *buf, size_t size);
-int tuner_device_6110_read_register(uint16_t reg, uint8_t *buf, size_t size);
-
 
 
 #endif /* _TUNER_DEVICE_H_ */
